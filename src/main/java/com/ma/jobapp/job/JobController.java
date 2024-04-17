31b87e83f160039bf0e654f8ee9ba63afd5f9ee3
“Mahmood <mahmoodadeel84@gmail.com>
2024-04-17 11:03:39 +0500

    delete job method added.

--- src/main/java/com/ma/jobapp/job/JobController.java
+++ src/main/java/com/ma/jobapp/job/JobController.java
@@ -7,6 +7,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,12 +38,19 @@ public class JobController {
     @GetMapping("/jobs/{id}")
     // uses responce entity with Job object to control over status codes
     public ResponseEntity<Job> getJobById(@PathVariable Long id) {
-    	
     	Job job = jobService.getJobById(id);
     	if(job != null)
     		return new ResponseEntity<Job> (job, HttpStatus.OK);
     	return new ResponseEntity<> (HttpStatus.NOT_FOUND);
-    	//return new Job(1L, "none", "none", "none", "none", "none");
+    }
+    
+    @DeleteMapping("jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id) {
+    	boolean deleted = jobService.deleteJobById(id);
+    	if(deleted)
+    		return new ResponseEntity<> ("Job deleted successfully", 
+    				HttpStatus.OK);
+    	return new ResponseEntity<> (HttpStatus.NOT_FOUND);
     }
 }
 
--- src/main/java/com/ma/jobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/ma/jobapp/job/impl/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ma.jobapp.job.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -34,4 +35,16 @@ public class JobServiceImpl implements JobService {
 		return null;
 	}
 
+	@Override
+	public boolean deleteJobById(Long id) {
+		Iterator<Job> iterator = jobs.iterator();
+		while (iterator.hasNext()) {
+			Job job = iterator.next();
+			if (job.getId().equals(id)) {
+				iterator.remove();
+				return true;
+			}
+		}
+		return false;
+	}
 }
