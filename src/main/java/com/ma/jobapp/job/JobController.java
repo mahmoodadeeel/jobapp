0f77ce02ae3f4f5e3a4f0848ae038c3e90016555
“Mahmood <mahmoodadeel84@gmail.com>
2024-04-17 10:38:20 +0500

    Add responce entity object fot handling http status codes.

--- src/main/java/com/ma/jobapp/job/JobController.java
+++ src/main/java/com/ma/jobapp/job/JobController.java
@@ -3,6 +3,10 @@ package com.ma.jobapp.job;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,20 +23,26 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+    	return new ResponseEntity<> (jobService.findAll(), HttpStatus.OK);
+        //return jobService.findAll();
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job added successfully";
+        return new ResponseEntity<> ("Job added successfully",HttpStatus.CREATED);
     }
     
     @GetMapping("/jobs/{id}")
-    public Job getJobById(@PathVariable Long id) {
+    // uses responce entity with Job object to control over status codes
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+    	
     	Job job = jobService.getJobById(id);
-    	return job;
+    	if(job != null)
+    		return new ResponseEntity<Job> (job, HttpStatus.OK);
+    	return new ResponseEntity<> (HttpStatus.NOT_FOUND);
+    	//return new Job(1L, "none", "none", "none", "none", "none");
     }
 }
 
